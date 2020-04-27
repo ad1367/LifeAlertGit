83776298424192853788ae09041fbdb7c9ab588f
ad1367 <ad1367@messiah.edu>
2020-04-27 18:00:51 -0400

    Uses 10 second timer after large acceleration to detect rest and activate second LED. Currently crashes when trying to use a while on the timer

--- LifeArduino/LifeArduino.ino
+++ LifeArduino/LifeArduino.ino
@@ -1,72 +1,68 @@
 const int ACCpin = A0;
-const int outputpin = 1;
-float ACC = 0;
-float ACCDC = 0;
-float ACCDC1 = 0;
-int count1 = 0;
-int count2 = 0;
-// create variable for the count1 window of time range
-int countrange1 = 100;
-//int countrange2 = countrange1/2 + countrange1;
-// find the value of acc at rest for DC offset fix
-int rest = 271;
-float ACCSUM1 = 0;
-float ACCSUM2 = 0;
-float ACCAVE1 = 0;
-float ACCAVE2 = 0;
-float MAX = 0;
-float MIN = -MAX;
-float ACC1MIN = 0;
-float ACC1MAX = 0;
-float dACC = 0;
+const int LED1 = 2;
+const int LED2 = 4;
+float ACC;
+float ACCDC;
+int rest;
+int MAX = 6;
+int ACCMAX = 0;
+int ACCMIN = 0;
+int dACC;
+int timestart;
+int timeelapsed;
+unsigned long time;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(ACCpin, INPUT);
-  
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
 }
 
 void loop() {
+  // start timer count in seconds
+  time = millis() / 1000;
   rest = analogRead(ACCpin);
   ACC = analogRead(ACCpin);
   ACCDC = ACC - rest;
-  Serial.print("ACCDC = ");
+  Serial.print("ACCDC: ");
   Serial.println(ACCDC);
 
-  //Double count, stagger to account for different sampling window
-  count1 = count1 + 1;
-  ACCSUM1 = ACCSUM1 + ACCDC;
-  
-  //count2 = count2 + 1 + (countrange1) / 2;
-  //ACCSUM2 = ACCSUM2 + ACCDC;
-
-  if (count1 < countrange1) {
-    count1 = 0;
-    ACC1MIN = min(ACCDC,500);
-    ACC1MAX = max(ACCDC,2000);
-    dACC= ACC1MAX - ACC1MIN;
-    Serial.print("Acceleration Sum: ");
-    Serial.print(ACCSUM1);
-    //reset ACCSUM
-    ACCSUM1 = 0;
+  // if acceleration trips a maximum magnitude, start fall timer
+  if (abs(ACCDC) > MAX) {
+    digitalWrite(LED1, HIGH);
+    timestart = time;
   }
-  
- // if (count2 > countrange2) {
-  //count2 = 0;
-  //Serial.print("Ave 2 = ");
-  //Serial.println(ACCSUM2);
-  //}
+  // initiate timer that starts after fall
+  timeelapsed = time - timestart;
+  Serial.print(" , ");
+  Serial.print("Timer: ");
+  Serial.println(timeelapsed);
 
-  if (ACCSUM1 > MAX or ACCSUM1 < MIN) {
-    digitalWrite(outputpin, HIGH);
- 
-  }
-  else {
-    digitalWrite(outputpin, LOW);
-  }
- 
-  
-  delay(10);
+  /* for 10 seconds after a potential fall has been detected,
+      sense for stillness of movement that eliminates walking
+      as a potential cause of  acceleration */
 
-  ACCAVE2 = 0;
+  /*while (timeelapsed < 10) {
+      if (ACCDC > ACCMAX) {
+        ACCMAX = ACCDC;
+        //Serial.print(" , ");
+        //Serial.print("Max ACC: ");
+        //Serial.println(ACCMIN);
+      }
+
+      if (ACCDC < ACCMIN) {
+        ACCMIN = ACCDC;
+        //Serial.print("Min ACC: ");
+        //Serial.println(ACCMIN);
+      }
+      // if the person is at rest for 10 seconds after jolt,
+      //   second LED alert will turn on
+      dACC = ACCMAX - ACCMIN;
+      if (dACC < 100) {
+        digitalWrite(LED2, HIGH);
+      }
+    }
+  */
+delay(10);
 }
